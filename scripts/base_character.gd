ac987c2f74f3a2a6ff1df16576222b1f65e39f3f
ubuntu-desktop <yili533@student.liu.se>
2026-09-18 03:29:59 +0200

    Made the code leaner and more scaleable, split stats from characters

--- scripts/base_character.gd
+++ scripts/base_character.gd
@@ -1,78 +1,71 @@
 class_name BaseCharacter
 extends CharacterBody2D
 
-# Possible character states
+# __________________________Unit Configs Properties__________________________
 enum State { IDLE, WALK, ATTACK, HURT, DEATH, SPECIAL }
+const ONCE_STATES := [State.ATTACK, State.SPECIAL, State.HURT, State.DEATH]
+const GAMESPEED = 30
 
-# Defensive stats
-@export var max_hp : int
-@export var hp : int
-@export var max_shield : int
-@export var shield : int
-@export var armor : int
+@export var stats : CharacterStats
+@export var team : Team.Side
 
-# Offensive stats
-@export var attack_damage : int
-@export var attack_speed : float
-@export var attack_range : int
-
-# Utility
-@export var movement_speed : float
-@export var max_energy : int
-@export var energy : int
-@export var team: Team.Side
-
-# Starting stats and configs
-const SPEED = 30
 var current_hp: int
-var current_energy: int
-var state: State = State.IDLE
-@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+var current_energy : int
+var state: State
+var target: BaseCharacter
+var starting_direction : Vector2
+var current_direction : Vector2
 
-# Direction
-var starting_direction: Vector2
-var current_direction: Vector2
-var direction : Vector2 = Vector2.ZERO
+@onready var animated_sprite : AnimatedSprite2D = $AnimatedSprite2D
+#@onready var hit_box : HitBox = $HitBox
 
+# __________________________Interaction signals__________________________
+signal character_damaged(current_hp: int)
+signal character_attack(damage: int)
+signal died()
 
-
-const DIR_2 = [ Vector2.LEFT, Vector2.RIGHT ]
-
-signal character_damaged()
-
-
-# Original hp, energy and state upon spawn
+# __________________________Spawn Config__________________________
 func _ready() -> void:
-	current_hp = max_hp
-	current_energy = max_energy
-	_set_state(State.IDLE)
-	_starting_facing(team)
+	# Checking if stats has been assigned for the character before being spawned.
+	if stats == null:
+		push_warning("%s has no Stats assigned — using defaults." % name)
+		stats = CharacterStats.new()
+	stats = stats.duplicate()
+	
+	animated_sprite.animation_finished.connect(_on_animation_finished)
+	_spawn()
 
+func _spawn() -> void:
+	current_hp = stats.max_hp
+	target = null
+	_set_state(State.IDLE)
+	_starting_direction()
+
+#__________________________Game Process__________________________
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	if current_hp <= 0:
-		_die()
+	# Stops animation after running once.
+	if state in ONCE_STATES:
 		return
-	if state == State.ATTACK:
-		return
-		
+	
+	# Temporary input controlled for testing
 	if Input.is_action_just_pressed("ui_accept"):
 		_attack()
 		velocity = Vector2.ZERO
 		move_and_slide()
 		return
-		
+	
 	var input_direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	if input_direction != Vector2.ZERO:
-		velocity = input_direction * movement_speed * SPEED
+		velocity = input_direction * stats.movement_speed * GAMESPEED
 		_walk()
-		_facing(input_direction)
+		_set_direction(input_direction)
 	else:
 		velocity = Vector2.ZERO
 		_idle()
 	move_and_slide()
 		
-	
+# __________________________Unit Animation__________________________
 # Animation depending on state
 func _set_state(new_state: State) -> void:
 	if state == new_state:
@@ -93,30 +86,45 @@ func _set_state(new_state: State) -> void:
 			animated_sprite.play("hurt")
 		State.DEATH:
 			animated_sprite.play("death")
-			
+
+func _on_animation_finished() -> void:
+	if state in [State.ATTACK, State.SPECIAL, State.HURT]:
+		_set_state(State.IDLE)
+	elif state == State.DEATH:
+		queue_free()
+
+# __________________________Unit interactions__________________________
 func _walk() -> void:
 	_set_state(State.WALK)
 
 func _attack() -> void:
 	_set_state(State.ATTACK)
-	animated_sprite.animation_finished.connect(_idle, CONNECT_ONE_SHOT)
+	character_attack.emit(stats.attack_damage)
 
 func _special() -> void:
 	_set_state(State.SPECIAL)
 	
-func _hurt() -> void:
-	_set_state(State.HURT)
-
+func _hurt(amount: int) -> void:
+	current_hp -= amount
+	character_damaged.emit(current_hp)
+	# No animation yet
+	#_set_state(State.HURT)
+	
+	# Checks if unit hp less than 0, if true run death animation.
+	if current_hp <= 0:
+		_death()
+	
 # Upon death, stop all logic and delete the character after playing the death animation once.
-func _die() -> void:
+func _death() -> void:
 	_set_state(State.DEATH)
 	set_physics_process(false)
-	animated_sprite.animation_finished.connect(func(): queue_free(), CONNECT_ONE_SHOT)
+	died.emit()
 
 func _idle() -> void:
 	_set_state(State.IDLE)
-	
-func _starting_facing(team: Team.Side) -> void:
+
+#__________________________Unit direction__________________________
+func _starting_direction() -> void:
 	if team == Team.Side.PLAYER:
 		starting_direction = Vector2.RIGHT
 		animated_sprite.flip_h = false
@@ -124,7 +132,7 @@ func _starting_facing(team: Team.Side) -> void:
 		starting_direction = Vector2.LEFT
 		animated_sprite.flip_h = true
 		
-func _facing(direction: Vector2) -> void:
+func _set_direction(direction: Vector2) -> void:
 	if direction.x > 0:
 		animated_sprite.flip_h = false
 		current_direction = Vector2.RIGHT
---
+++ scripts/base_character_stats.gd
@@ -0,0 +1,20 @@
+class_name CharacterStats
+extends Resource
+
+@export_group("Defensive")
+@export var max_hp : int
+@export var hp : int
+@export var max_shield : int
+@export var shield : int
+@export var armor : int
+
+@export_group("Offensive")
+@export var attack_damage: int
+@export var attack_speed: float
+@export var attack_range: int
+
+@export_group("Utility")
+@export var price : int
+@export var movement_speed : float
+@export var max_energy : int
+@export var energy : int
